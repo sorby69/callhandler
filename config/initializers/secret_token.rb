444b2771293c7f4599f00ff6e76d0bab88d09f47
Rob Dunsford <rob.dunsford@thinkidea.co.uk>
2012-11-17 09:52:00 +0000

    Base commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callhandler::Application.config.secret_token = 'cd92b2f6846d943c02d279f1e0310cc38714f95287d195bd824ff9ae357c9aa7163e323cd46130a80568d8b5c50513ad7e5685ba1f24f01732691c04a0193182'
